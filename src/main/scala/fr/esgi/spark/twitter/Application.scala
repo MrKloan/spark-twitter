f5ec8f6487885f99169b23fb4487e6e73c54be6d
Valentin Fries <contact@fries.io>
2018-06-07 12:02:35 +0200

    Country segregation

--- src/main/scala/fr/esgi/spark/twitter/Application.scala
+++ src/main/scala/fr/esgi/spark/twitter/Application.scala
@@ -1,9 +1,30 @@
 package fr.esgi.spark.twitter
 
-import fr.esgi.spark.twitter.Application.frenchTweets
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Minutes, StreamingContext}
+import twitter4j.Status
+
+final class Country(name: String, code: String, lang: String) {
+
+  def isSentFrom(tweet: Status): Boolean = {
+    val place = tweet.getPlace
+    val user = tweet.getUser
+
+    val isCountry = place != null && place.getCountry != null && place.getCountry.eq(this.name)
+    val isCountryCode = place != null && place.getCountryCode != null && place.getCountryCode.eq(this.code)
+    val isUserLocation = user != null && user.getLocation != null && (user.getLocation.contains(this.name) || user.getLocation.eq(this.code))
+
+    isCountry || isCountryCode || isUserLocation
+  }
+
+  def isLang(tweet: Status): Boolean = {
+    val isTweetLang = tweet.getLang != null && tweet.getLang.eq(this.lang)
+    val isUserLang = tweet.getUser != null && tweet.getUser.getLang != null && tweet.getUser.getLang.eq(this.lang)
+
+    isTweetLang || isUserLang
+  }
+}
 
 object Application extends App {
 
@@ -16,22 +37,25 @@ object Application extends App {
   val streamingContext = new StreamingContext(sparkConf, Minutes(1))
   val twitterStream = TwitterUtils.createStream(streamingContext, None /*, Array("#Spark")*/)
 
-  val tweetsWithPlace = twitterStream.filter(_.getPlace != null)
-  val frenchTweets = tweetsWithPlace.filter(_.getPlace.getCountryCode equals "FR")
-  val usTweets = tweetsWithPlace.filter(_.getPlace.getCountryCode equals "US")
+  val france = new Country("France", "FR", "fr")
+  val usa = new Country("United States", "US", "en")
 
-  val frenchRT = frenchTweets
-    .filter(_.isRetweeted)
-    .filter(_.getRetweetedStatus.getPlace != null)
-    .filter(_.getRetweetedStatus.getPlace.getCountryCode equals "US")
+  val frTweets = twitterStream.filter(france.isSentFrom)
+  val frRt = frTweets.filter(_.isRetweet).filter(_.getRetweetedStatus != null)
+  val frenchRetweetingUs = frRt.filter(tweet => usa.isSentFrom(tweet.getRetweetedStatus))
+  val frenchRetweetingEnglish = frRt.filter(tweet => usa.isLang(tweet.getRetweetedStatus))
+  val frenchRetweetingFrench = frRt.filter(tweet => france.isLang(tweet.getRetweetedStatus))
 
-  val usRT = usTweets
-    .filter(_.isRetweeted)
-    .filter(_.getRetweetedStatus.getPlace != null)
-    .filter(_.getRetweetedStatus.getPlace.getCountryCode equals "FR")
+  val usTweets = twitterStream.filter(usa.isSentFrom)
+  val usRt = usTweets.filter(_.isRetweet).filter(_.getRetweetedStatus != null)
+  val usRetweetingFr = usRt.filter(tweet => france.isSentFrom(tweet.getRetweetedStatus))
+  val usRetweetingFrench = usRt.filter(tweet => france.isLang(tweet.getRetweetedStatus))
 
-  frenchRT.print()
-  usRT.print()
+  frenchRetweetingFrench.count().map(count => count + " FR RT French").print()
+  frenchRetweetingUs.count().map(count => count + " FR RT US").print()
+  frenchRetweetingEnglish.count().map(count => count + " FR RT English").print()
+  usRetweetingFr.count().map(count => count + " US RT FR").print()
+  usRetweetingFrench.count().map(count => count + " US RT French").print()
 
   streamingContext.start()
   streamingContext.awaitTermination()
